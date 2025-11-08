b374621de77ab5697ac3c6c0cbac6a5d5e8ca013
rayyansurani <rayyan.surani@hotmail.com>
2025-11-08 18:41:24 -0500

    Fixed inference

--- backend/services/yolo_detector.py
+++ backend/services/yolo_detector.py
@@ -1,22 +1,24 @@
 # yolo_detector.py
 from ultralytics import YOLO
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Load YOLO model once at import
 MODEL_PATH = os.getenv("YOLO_MODEL_PATH")
-model = YOLO(MODEL_PATH)
+model = YOLO(MODEL_PATH, verbose=False)
 
 
 def yolo_detect(image_path: str):
     # Run YOLO inference
-    results = model.predict(image_path, verbose=False)
-    result = results[0]
+    results = model.predict(image_path, verbose=False)[0]
 
     objects = []
     for box in results.boxes:
         cls_id = int(box.cls[0])
         conf = float(box.conf[0])
-        x1, y1, x2, y2 = box.xyxy[0]  # tensor -> [x1, y1, x2, y2]
+        x1, y1, x2, y2 = box.xyxy[0].tolist() # tensor -> [x1, y1, x2, y2]
         objects.append({
             "class": results.names[cls_id],
             "confidence": round(conf, 3),
--- object_detection/inference/yolo_infer.py
+++ object_detection/inference/yolo_infer.py
@@ -1,6 +1,11 @@
 # yolo_camera_detect.py
+import time
+
 import cv2
+from matplotlib import pyplot
 from ultralytics import YOLO
+from backend.services.yolo_detector import yolo_detect
+
 
 def getObjects(image):
     model = YOLO("../model/yolov8n.pt")
@@ -10,7 +15,7 @@ def getObjects(image):
     for box in r.boxes:
         cls_id = int(box.cls)
         conf = float(box.conf)
-        x1, y1, x2, y2 = box.xyxy[0]  # tensor -> [x1, y1, x2, y2]
+        x1, y1, x2, y2 = box.xyxy  # tensor -> [x1, y1, x2, y2]
         objects.append({r.names[cls_id], conf, x1, y1, x2, y2})
 
     return {"Objects": objects}
@@ -18,7 +23,7 @@ def getObjects(image):
 
 def main():
     # Load a YOLOv8 model (pretrained or your custom model)
-    model = YOLO("../model/yolov8n.pt")  # or 'best.pt' if you've trained your own
+    model = YOLO("../model/yolov8n.pt", verbose=False)  # or 'best.pt' if you've trained your own
 
     # Open webcam (0 = default camera)
     cap = cv2.VideoCapture(0)
@@ -30,13 +35,15 @@ def main():
     print("Press 'q' to quit.")
 
     while True:
+        time.sleep(5)
+
         ret, frame = cap.read()
         if not ret:
             print("Error: Failed to read frame.")
             break
-
+        print(yolo_detect(frame))
         # Run YOLO inference on the frame
-        results = model(frame)
+        results = model(frame, verbose=False)
 
         # Draw detection boxes on the frame
         annotated_frame = results[0].plot()
@@ -51,6 +58,7 @@ def main():
     # Release camera and close window
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
