8e789bf384c805e770283359faf3655d1d495512
LegitBaller0243 <samjswag@gmail.com>
2025-11-08 14:51:00 -0500

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from routes import routes
+
+
+def create_app():
+    """Create and configure the Flask application."""
+    app = Flask(__name__)
+    app.register_blueprint(routes)
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port=5000, host="0.0.0.0")
