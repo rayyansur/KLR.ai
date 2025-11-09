e56feb31bb8e7d69d2611e5b48696d6fefa0aeec
LegitBaller0243 <samjswag@gmail.com>
2025-11-08 19:17:27 -0500

    starting front end

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from routes import routes
-
+from CORS import CORS
 
 def create_app():
     """Create and configure the Flask application."""
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     app.register_blueprint(routes)
     return app
 
